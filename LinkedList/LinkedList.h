f48d63ebba7653d6bff941e9d1eed8aa24be905d
Zakaria <zakaria.soliman1@gmail.com>
2012-07-19 20:49:28 -0400

    Put all the function definitions at the end of the .h file. The class interface is no more readable

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -1,10 +1,9 @@
-#ifndef _LINKEDLIST_H_
-#define	_LINKEDLIST_H_
-
+#ifndef LINKEDLIST_H
+#define	LINKEDLIST_H
 #include<iostream>
 #include<iterator>
 
-
+//Forward declaration because I will use the list class in the node class.
 template<typename T> class LinkedList;
 
 ///*******Node class*******///
@@ -18,70 +17,100 @@ private:
 	ListNode<T>* next;
 };
 
-
 template <typename T>
 class LinkedList
 {
+private:
+	ListNode<T>* head;		//keeps track of the first element of the list
+	ListNode<T>* tail;		//keeps traks of the last element in the list
+	unsigned int length;	//length of the list
+
 public:
-	
 	////*********Iterator Class*********////
 	class ListIterator : public std::iterator<std::forward_iterator_tag, T>
 	{
 	public:
-		ListIterator():currPos(NULL) {};
-		//operators
+		ListIterator():currPos(NULL) {};		
+		//operators//
 		void operator++() {currPos = currPos->next;};
 		void operator++(int) {currPos = currPos->next;};
-		bool operator!=(ListIterator rval){ return (currPos != rval.currPos); }
-		bool operator==(ListIterator rval){ return (currPos == rval.currPos); }
+		bool operator!=(ListIterator rval) { return (currPos != rval.currPos); }
+		bool operator==(ListIterator rval) { return (currPos == rval.currPos); }
 		T operator*() {return currPos->next->data;};
 		T* operator->() {return &(currPos->next->data);};
-		ListIterator& operator=(ListIterator& rval)
-		{
+		ListIterator& operator=(ListIterator& rval){
 			if (this->currPos == rval.currPos)
 				return *this;
 			this->currPos = rval.currPos;
 			return *this;
 		};
-
 	private:
 		ListNode<T>* currPos;
 		friend class LinkedList<T>;
+		//This constructor is private so that users only access the default constructor
 		ListIterator(ListNode<T>* value):currPos(value) {};
 	};
+	////*********END*********////
+	
+	LinkedList(void); //Constructor
+	~LinkedList(void) { DeleteList(); delete head; }; //Destructor
+
+	//*** List Manipulation Methods ***//
+
+	//Removes the element that is pointed to by the iterator
+	void Remove(const ListIterator& it);
+	//inserts an element after our position, returns false if it fails
+	bool InsertAfter(const ListIterator& it, const T& data);
+	//inserts an element before our current position, returns false if it fails
+	void InsertBefore(const ListIterator& it, const T& data);
+	//remove first element of the list
+	void DeleteHead(void);
+	//deletes the entire list
+	void DeleteList(void);
+	//Add an element to the front of the list, returns false if it fails
+	bool PushFront(const T& data);
+	//Add an element to the back of the list, returns false if it fails
+	bool PushBack(const T& data);
+
+	//********Accessors********//
+
+	unsigned int GetLength(void) {return length;};
+	bool IsEmpty(void) { return (length == 0); };
+	T FirstElement() {return head->next->data;};
+
+	//** Get relevant positions **//
+
+	ListIterator begin(void) {return ListIterator(head); };
+	ListIterator end(void) {return ListIterator(tail); };
+};
 
 
-	//Constructor
-	LinkedList(void): length(0)
+template <typename T>
+LinkedList<T>::LinkedList(void): length(0)
 	{
 		try{
 			head = new ListNode<T>;
 			tail = head;
 			head->next = NULL;
 		}catch(bad_alloc& exc){
-			cout<<"Memory allocation failed";
+			cerr<<"Memory allocation failed, cannot initilize the linked list.";
 		}
-	};
+	}
 
-	//Destructor
-	~LinkedList(void) {DeleteList(); delete head;};
+template <typename T>
+void LinkedList<T>::Remove(const ListIterator& it)
+{
+	if (!IsEmpty()){
+		ListNode<T>* temp = NULL;
+		temp = it.currPos->next;
+		it.currPos->next = it.currPos->next->next;
+		delete temp;
+		--length;
+	}
+}
 
-
-	//*** List Manipulation Methods ***//
-	//Removes the element that is pointed to by the iterator
-	void Remove(const ListIterator& it)
-	{
-		if (!IsEmpty()){
-			ListNode<T>* temp = NULL;
-			temp = it.currPos->next;
-			it.currPos->next = it.currPos->next->next;
-			delete temp;
-			--length;
-		}
-	};
-
-	//inserts an element after our position, returns false if it fails
-	bool InsertAfter(const ListIterator& it, const T& data)
+template<typename T>
+bool LinkedList<T>::InsertAfter(const ListIterator& it, const T& data)
 	{
 		if (!IsEmpty()){
 			try {
@@ -97,10 +126,10 @@ public:
 			}
 		}else
 			return;
-	};
+	}
 
-	//inserts an element before our current position, returns false if it fails
-	void InsertBefore(const ListIterator& it, const T& data)
+template<typename T>
+void LinkedList<T>::InsertBefore(const ListIterator& it, const T& data)
 	{
 		if (!IsEmpty()){
 			try {
@@ -115,19 +144,19 @@ public:
 			}else
 				return;
 		}
-	};
+}
 
-	//remove first element of the list
-	void DeleteFirst(void)
+template<typename T>
+void LinkedList<T>::DeleteHead(void)
 	{
 		ListNode<T>* temp;
 		temp = head->next;
 		head->next = head->next->next;
 		delete temp;
-	};
+	}
 
-	//deletes the entire list
-	void DeleteList(void)
+template<typename T>
+void LinkedList<T>::DeleteList(void)
 	{
 		ListNode<T>* temp;
 		while(head->next != NULL)
@@ -137,11 +166,10 @@ public:
 			delete temp;
 			--length;
 		}
-	};
+	}
 
-
-	//Add an element to the front of the list, returns false if it fails
-	bool PushFront(const T& data)
+template<typename T>
+bool LinkedList<T>::PushFront(const T& data)
 	{
 		try {
 			ListNode<T>* newNode = new ListNode<T>;
@@ -158,10 +186,10 @@ public:
 		}catch(bad_alloc& exc){
 				return false;
 			}
-	};
+	}
 
-	//Add an element to the back of the list, returns false if it fails
-	bool PushBack(const T& data)
+template <typename T>
+bool LinkedList<T>::PushBack(const T& data)
 	{
 		try {
 			ListNode<T>* newNode = new ListNode<T>;
@@ -181,22 +209,6 @@ public:
 		}catch(bad_alloc& exc){
 				return false;
 			}
-	};
+	}
 
-	//********Accessors********//
-	unsigned int GetLength(void) {return length;};
-	bool IsEmpty(void) { return (length == 0); };
-	T FirstElement() {return head->next->data;};
-
-	//** Get relevant positions **//
-	ListIterator begin(void) {return ListIterator(head); };
-	ListIterator end(void) {return ListIterator(tail); };
-
-private:
-
-	ListNode<T>* head;		//keeps track of the first element of the list
-	ListNode<T>* tail;		//keeps traks of the last element in the list
-	unsigned int length;	//length of the list
-
-};
 #endif
